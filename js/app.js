2cbf58c9cda127943b8ad7832e08a395268fb8e5
Price Comstock <price.comstock@gmail.com>
2017-03-12 15:41:21 -0500

    Game now working. Movement is possible.

--- js/app.js
+++ js/app.js
@@ -5,20 +5,28 @@ var game = new Phaser.Game(800,600, Phaser.AUTO, '', {
 },
 false, false);
 
+var C = {
+    movespeed: 200
+};
+
+
 var player;
 var cursors;
 
 function preload() {
-    game.load.spritesheet('dragon', 'assets/dragon.png', 32, 32);
+    game.load.spritesheet('dragon', 'assets/dragon.png', 32, 32, 6);
 }
 
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     player = game.add.sprite(0, 100, 'dragon');
-    player.scale.setTo(7,7);
+    player.scale.setTo(5,5);
+
     player.enableBody = true;
+    game.physics.arcade.enable(player);
     player.body.collideWorldBounds = true;
+    console.log(player)
 
     cursors = game.input.keyboard.createCursorKeys();
 }
@@ -27,9 +35,9 @@ function update() {
     player.body.velocity.y = 0;
 
     if (cursors.up.isDown) {
-        player.body.velocity.y = -100;
+        player.body.velocity.y = -C.movespeed;
     } else if (cursors.down.isDown) {
-        player.body.velocity.y = 100;
+        player.body.velocity.y = C.movespeed;
     } else {
         player.animations.stop();
     }
